f8fea7c6d9042dc6303388285e219939de6b7ef4
shuais <shuai.s@northeastern.edu>
2022-08-10 10:50:47 -0700

    add global files

---
+++ globalFile.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// admin file
+#define ADMIN_FILE "admin.txt"
+// student file
+#define STUDENT_FILE "student.txt"
+// teacher file
+#define TEACHER_FILE "teacher.txt"
+// computer room file
+#define COMPUTER_FILE "computerRoom.txt"
+// order file
+#define ORDER_FILE "order.txt"
