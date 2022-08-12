8d20e4bfdaddec98ad1988f1bd8e44f8a057d40a
shuais <shuai.s@northeastern.edu>
2022-08-11 20:30:27 -0700

    impl show all order

--- student.cpp
+++ student.cpp
@@ -148,7 +148,7 @@ void Student::showMyOrder()
             string status;
             if (of.orderData[i]["status"] == "-1")
             {
-                status = "Submitted";
+                status = "Rejected";
             }
             else if (of.orderData[i]["status"] == "0")
             {
@@ -176,6 +176,45 @@ void Student::showMyOrder()
 // view all reservation
 void Student::showAllOrder()
 {
+    OrderFile of;
+
+    if (of.size == 0)
+    {
+        cout << "No data" << endl;
+        return;
+    }
+
+    string dates[] = {"placeholer", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+
+    for (int i = 0; i < of.size; i++)
+    {
+
+        string status;
+        if (of.orderData[i]["status"] == "-1")
+        {
+            status = "Rejected";
+        }
+        else if (of.orderData[i]["status"] == "0")
+        {
+            status = "Canceled";
+        }
+        else if (of.orderData[i]["status"] == "1")
+        {
+            status = "Submitted";
+        }
+        else
+        {
+            status = "Approved";
+        }
+
+        cout << i + 1 << ". "
+             << "\tDate: " << dates[atoi(of.orderData[i]["date"].c_str())]
+             << "\tInterval: " << (of.orderData[i]["interval"] == "1" ? "AM" : "PM")
+             << "\tID: " << of.orderData[i]["stuId"]
+             << "\tName: " << of.orderData[i]["stuName"]
+             << "\tRoom: " << of.orderData[i]["roomId"]
+             << "\tStatus: " << status << endl;
+    }
 }
 
 // cancel reservation
